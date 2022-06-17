f3a617e13a4e4917139bf95976b122b8eb74a537
Mohammed Kamaldeen <mohammedkamaldeen204@gmail.com>
2022-06-17 07:36:20 +0100

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // new line
+const ESLintPlugin = require('eslint-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'eval-source-map',  // new line
+  devServer: {                 // new line
+    static: './dist'      // new line
+  },
+
+  plugins: [
+    new CleanWebpackPlugin(), // new line
+    new ESLintPlugin({
+      context: "compiler.context",
+      eslintPath: "eslint",
+      extensions: 'js',
+      exclude: 'node_modules',
+      fix: false,
+      formatter: 'stylish',
+      lintDirtyModulesOnly: false,
+      threads: false,
+      emitError: true,
+      emitWarning: true,
+      failOnError: true,
+      failOnWarning: false,
+      quiet: false,
+      outputReport: false
+    }),
+    new HtmlWebpackPlugin({
+      title: 'Weather',
+      template: './src/index.html',
+      inject: 'body'
+    }),
+    new Dotenv()
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+
+};
